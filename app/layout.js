ab0c88646042a0260d5bbd0bd27acb08b3438ee4
UtkarshVerma-Coder <utkarshverma001@outlook.com>
2023-10-22 02:48:10 +0530

    added progressbar

--- app/layout.js
+++ app/layout.js
@@ -1,7 +1,7 @@
 import './globals.css'
 import { Inter } from 'next/font/google'
 import Nav from '@/components/BottomNav/nav'
-
+import NextTopLoader from 'nextjs-toploader';
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -23,6 +23,7 @@ export default function RootLayout({ children}) {
     <html lang="en">
 
       <body className={inter.className}>
+      <NextTopLoader />
         {children}
       {/*Nav Bar On Bottom  */}
       <Nav/>
