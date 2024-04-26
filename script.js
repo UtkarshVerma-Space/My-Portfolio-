0adc80797a26e5b34e2a6ba68ab2608ac29b81b5
UtkarshVerma-Space <utkarshverma001@outlook.com>
2024-04-26 12:51:52 +0530

    Minor Chnages

--- script.js
+++ script.js
@@ -17,3 +17,14 @@ const target = document.querySelector('#js-target');
 let scrolll = ()=>{
     scroll.scrollTo(target)
 }
+
+
+
+
+
+
+
+  Shery.hoverWithMediaCircle(".hover-target" /* Element to target.*/, {
+    images: ["image1.jpg", "image2.jpg", "image3.jpg"] /*OR*/,
+    //videos: ["video1.mp4", "video2.mp4"],
+  })
