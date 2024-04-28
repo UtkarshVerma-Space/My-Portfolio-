18443c175f17434a15c076d31e31faae18f8dacf
UtkarshVerma-Space <utkarshverma001@outlook.com>
2024-04-28 20:12:23 +0530

    U[date new animation

--- script.js
+++ script.js
@@ -61,32 +61,149 @@ gsap.from("#abt p", {
 
 
 
-// Tools And Skills
 
 
-gsap.from("#skills h1", {
-    y: 15,
+
+
+
+
+
+// skills
+gsap.from("#skills #label", {
+    y: 55,
     opacity: 0,
     scrollTrigger: {
-        trigger: "#skills", // Define the trigger element
-        start: "top 90%", // Trigger animation when the element is 80% from the top of the viewport
-        scroller:"body",
-        scrub:2
+        trigger:"#skills",
+        scrub:true,
+        start:"top 10%",
+        pin:true
+    }
+});
+gsap.from("#skills #mongo", {
+    y: 55,
+    opacity: 0,
+    scrollTrigger: {
+        trigger:"#skills",
+        scrub:true,
+        start:"top 10%",
+    }
+});
+
+gsap.from("#skills #express", {
+    y: 55,
+    opacity: 0,
+    scrollTrigger: {
+        trigger:"#skills",
+        scrub:true,
+        start:"top 10%"
+    }
+});
+gsap.from("#skills #react", {
+    y: 55,
+    opacity: 0,
+    scrollTrigger: {
+        trigger:"#skills",
+        scrub:true,
+        start:"top 10%"
+    }
+});
+gsap.from("#skills #node", {
+    y: 55,
+    opacity: 0,
+    scrollTrigger: {
+        trigger:"#skills",
+        scrub:true,
+        start:"top 10%"
     }
 });
 
 
 
-gsap.from("#skills i", {
-    y: 15,
-    opacity:0,
+
+
+// skills Desktop
+gsap.from("#skills-desktop #label", {
+    y: 105,
+    opacity: 0,
     scrollTrigger: {
-        trigger: "#skills", // Define the trigger element
-        start: "top 90%", // Trigger animation when the element is 80% from the top of the viewport
-        scroller:"body",
-        scrub:2,
-    },
+        trigger:"#skills-desktop",
+        scrub:true,
+        start:"top 100%",
+        pin:true
+    }
 });
+gsap.from("#skills-desktop #mongo", {
+    y: 455,
+    opacity: 0,
+    scrollTrigger: {
+        trigger:"#skills-desktop",
+        scrub:true,
+        start:"top 80%",
+    }
+});
+
+gsap.from("#skills-desktop #express", {
+    y: 155,
+    opacity: 0,
+    scrollTrigger: {
+        trigger:"#skills-desktop",
+        scrub:true,
+        start:"top 80%"
+    }
+});
+gsap.from("#skills-desktop #react", {
+    y: 155,
+    opacity: 0,
+    scrollTrigger: {
+        trigger:"#skills-desktop",
+        scrub:true,
+        start:"top 80%"
+    }
+});
+gsap.from("#skills-desktop #node", {
+    y: 55,
+    opacity: 0,
+    scrollTrigger: {
+        trigger:"#skills-desktop #node",
+        scrub:true,
+        start:"top 80%",
+
+    }
+});
+
+
+
+
+
+
+gsap.from("#knowledge  #frontend", {
+    y:100,
+    opacity:0.5,
+    marginTop:"500px",
+    scrollTrigger: {
+        trigger:"#knowledge",
+        scrub:1,
+        start:"top 10%",
+    }
+})
+
+gsap.from("#knowledge  #backend", {
+    y:100,
+    opacity:0,
+    marginTop:"500px",
+    scrollTrigger: {
+        trigger:"#frontend",
+        scrub:1,
+    }
+})
+
+
+
+
+
+
+
+
 
 
 
@@ -100,6 +217,7 @@ gsap.to("#projects  #img", {
         trigger:"#projects",
         scrub:true,
         start:"end 100%",
+        markers:true,
+        start:"end 300%%"
     }
 })
-
